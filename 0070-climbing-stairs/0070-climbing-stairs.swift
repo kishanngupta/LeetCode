1a40c7d3a97f0618fa609d025b877ff90a265999
Kishan Gupta <g.kis905@gmail.com>
2024-01-06 22:14:09 +0530

    Time: 0 ms (100.00%), Space: 15.1 MB (6.40%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.swift
+++ 0070-climbing-stairs/0070-climbing-stairs.swift
@@ -1,25 +1,18 @@
 class Solution {
-    /// Solving it using the memoization
+    /// Solving it using the tabular
     var dp: [Int] = []
     
     func climbStairs(_ n: Int) -> Int {
         dp = Array(repeating: -1, count: n+1)
-        climb(n)
-        return dp[n]
+        
+        for i in (0..<n+1).reversed() {
+            if i == n || i == n-1 {
+                dp[i] = 1
+            } else {
+                dp[i] = dp[i+1] + dp[i+2]
+            }
+        }
+        return dp[0]
     }
     
-    private func climb(_ n: Int) -> Int {
-        if n == 0 { return 1 }
-        else if n < 0 { return 0 }
-         
-        if dp[n] != -1 {
-            return dp[n]
-        }
-        
-        let left = climb(n-1)
-        let right = climb(n-2)
-        dp[n] = left + right
-        
-        return dp[n]
-    }
 }
