9ddb02ca10c0cb09f91396433697cf2bf1088873
Kishan Gupta <g.kis905@gmail.com>
2024-01-09 11:49:23 +0530

    Time: 5 ms (18.82%), Space: 15 MB (18.82%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.swift
@@ -0,0 +1,19 @@
+class Solution {
+    /// Solved using tabular method
+    var dp: [[Int]] = []
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+
+        dp = Array(
+            repeating: Array(
+                repeating: 1, 
+                count: n),
+            count: m)
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            }
+        }
+        return dp[m-1][n-1]
+    }
+}
