4b124a7a1c5a3e0d9eea7648123c5ef7f2325b20
Kishan Gupta <g.kis905@gmail.com>
2024-01-10 22:23:50 +0530

    Time: 124 ms (7.41%), Space: 16 MB (7.41%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func minFallingPathSum(_ matrix: [[Int]]) -> Int {
+        var mini = Int.max
+        let m = matrix.count
+        let n = matrix[0].count
+        
+        var dp = Array(repeating: Array(repeating: -1, count: n), count: m)
+        
+        for i in (0..<m).reversed() {
+            for j in (0..<n).reversed() {
+                if i == m-1 {
+                    dp[i][j] = matrix[i][j]
+                } else if j == 0{
+                    dp[i][j] = matrix[i][j] + min(dp[i+1][j], dp[i+1][j+1])
+                } else if j == n-1 {
+                    dp[i][j] = matrix[i][j] + min(dp[i+1][j], dp[i+1][j-1])
+                } else {
+                    var minimum = min(dp[i+1][j-1], dp[i+1][j])
+                    minimum = min(minimum, dp[i+1][j+1])
+                    dp[i][j] = matrix[i][j] + minimum
+                }
+                if i == 0 {
+                    mini = min(mini, dp[i][j])
+                }
+            }
+        }
+        
+        return mini
+    }
+}
