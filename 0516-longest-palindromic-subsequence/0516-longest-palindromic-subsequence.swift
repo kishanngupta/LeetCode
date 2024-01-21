4cd030b037443c424b9665adfe7156694221ce6c
Kishan Gupta <g.kis905@gmail.com>
2024-01-21 18:29:39 +0530

    Time: 75 ms (31.03%), Space: 15.8 MB (93.10%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.swift
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.swift
@@ -3,21 +3,24 @@ class Solution {
         var n = s.count
         var s1 = Array(s)
         var s2 = Array(s.reversed())
-        var dp:[[Int]] = Array(repeating: Array(repeating: 0, count: n+1), count: n+1)
+        
+        var prev = Array(repeating: 0, count: n+1)
+        var cur = Array(repeating: 0, count: n+1)
         
         for i in 1...n {
             for j in 1...n {
                 if s1[i-1] == s2[j-1] {
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    cur[j] = 1 + prev[j-1]
                 } else {
-                    var first = dp[i-1][j]
-                    var second = dp[i][j-1]
-                    dp[i][j] = max(first, second)
+                    var first = prev[j]
+                    var second = cur[j-1]
+                    cur[j] = max(first, second)
                 }
             }
+            prev = cur
         }
         
-        return dp[n][n]
+        return cur[n]
     }
     
     private func calculate(_ i: Int,_ j: Int, _ s1: [Character],_ s2: [Character],_ dp:inout [[Int]]) -> Int {
