f4dd4b4bb872dda0cae3b1dac1ef29ec1459f4f0
Kishan Gupta <g.kis905@gmail.com>
2024-01-24 22:24:52 +0530

    Time: 34 ms (7.96%), Space: 17.5 MB (12.41%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.swift
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.swift
@@ -1,10 +1,29 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
+        let n = prices.count
         /// Its a Dp of 2D array
         /// first array represents the list of prices
         /// second represents the option to buy or not /// it's a bool - 0 or 1
-        var dp: [[Int]] = Array(repeating: Array(repeating: -1, count: 2), count: prices.count)
-        return profit(0,1, prices, &dp)
+        var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: 2), count: n+1)
+        dp[n][0] = 0
+        dp[n][1] = 0
+
+        for i in (0..<n).reversed() {
+            for j in (0..<2) {
+                if j == 1 {
+                    let buy = -prices[i] + dp[i+1][0]
+                    let notBuy = dp[i+1][1]
+                    dp[i][j] = max(buy, notBuy)
+                } else {
+                    let sold = prices[i] + dp[i+1][1]
+                    let notSell = dp[i+1][0]
+                    dp[i][j] = max(sold, notSell)
+                }
+            }
+        }
+        
+        return dp[0][1]
+        // return profit(0,1, prices, &dp)
     }
     
     private func profit(_ i: Int,_ j: Int,_ prices: [Int],_ dp: inout [[Int]]) -> Int {
