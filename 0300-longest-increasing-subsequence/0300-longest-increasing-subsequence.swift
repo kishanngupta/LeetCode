5e297b95daa3679434f4c5e2704cc36e92f84b6a
Kishan Gupta <g.kis905@gmail.com>
2024-02-02 10:10:02 +0530

    Time: 616 ms (5.10%), Space: 64.5 MB (6.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.swift
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.swift
@@ -1,21 +1,22 @@
 class Solution {
     func lengthOfLIS(_ nums: [Int]) -> Int {
-        /// Initialize an array dp with the same length as nums and fill it with 1s.
-        var dp: [Int] = Array(repeating: 1, count: nums.count)
-        var maxi = 1
-        ///Iterate through each element in nums.
-        for i in 0..<nums.count {
-            ///Iterate through all prev element till current index
-            for j in 0..<i {
-                /// if prev index element is smaller then current index element
-                /// Update dp at current index, with maximum of value of prev index element + 1, if its greater than current index value of dp
-                if nums[i] > nums[j] {
-                    dp[i] = max(dp[i], dp[j]+1)
-                    /// compute the maximum at the same time
-                    maxi = max(maxi, dp[i])
-                }
-            }
+        let n = nums.count
+        var dp: [[Int]] = Array(repeating: Array(repeating: -1, count: n), count: n)
+        return length(0, -1, nums, &dp)
+    }
+    
+    private func length(_ index: Int,_ prev:Int,_ nums: [Int],_ dp: inout [[Int]]) -> Int {
+        if index == nums.count { return 0 }
+        else if dp[index][prev+1] != -1 {
+            return dp[index][prev+1]
         }
-        return maxi
+        
+        let notPick = length(index+1, prev, nums, &dp)
+        var pick = 0
+        if prev == -1 || nums[prev] < nums[index] {
+            pick = 1 + length(index+1, index, nums, &dp)
+        }
+        dp[index][prev+1] = max(pick, notPick)
+        return dp[index][prev+1]
     }
 }
