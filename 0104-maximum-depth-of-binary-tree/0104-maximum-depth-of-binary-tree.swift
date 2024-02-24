8620f3a551308839d9e8584ed218ead517cccf4b
Kishan Gupta <g.kis905@gmail.com>
2024-02-24 15:13:30 +0530

    Time: 13 ms (82.57%), Space: 15.8 MB (74.73%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.swift
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+
+        var queue: [TreeNode] = [root!]
+        var height = 0
+        
+        while (!queue.isEmpty) {
+            for _ in queue {
+                let node = queue.removeFirst()
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            height += 1
+        }
+        return height
+        
+    }
+}
