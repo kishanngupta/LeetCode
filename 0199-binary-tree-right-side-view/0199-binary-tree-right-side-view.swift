4c3a6b489b7113992192b15bd7ae2ed94759f7b4
Kishan Gupta <g.kis905@gmail.com>
2024-02-26 16:48:35 +0530

    Time: 3 ms (86.79%), Space: 15.8 MB (34.97%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        if root == nil { return [] }
+        var queue: [TreeNode] = [root!]
+        var result: [Int] = []
+        
+        while !queue.isEmpty {
+            var temp = queue
+            for index in 0..<temp.count {
+                let node = queue.removeFirst()
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+                if index == temp.count-1 {
+                    result.append(temp.last!.val)
+                }
+                
+            }
+        }
+        
+        return result
+    }
+}
