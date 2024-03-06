f86b115bbff1da32780194b6179096a17a19d596
Kishan Gupta <g.kis905@gmail.com>
2024-03-07 00:10:53 +0530

    Time: 69 ms (93.71%), Space: 18.5 MB (6.99%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.swift
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.swift
@@ -16,16 +16,21 @@
 
 class BSTIterator {
     var root: TreeNode?
+    /// this stack will store the tree-nodes
+    /// note - we are trying to replicate the recursion behavior with the help of stack
     var stack: [TreeNode] = []
 
     init(_ root: TreeNode?) {
         self.root = root
+        /// add root and its all left children
         addLeft(root)
     }
 
     private func addLeft(_ root: TreeNode?) {
+        /// check root is not null
         if root == nil { return }
         var head = root
+        /// add root and its all left children
         while(head != nil) {
             stack.append(head!)
             head = head?.left
@@ -33,12 +38,16 @@ class BSTIterator {
     }
     
     func next() -> Int {
+        /// remove the last added element from stack
         let node = stack.removeLast()
+        /// add right node and its all left children in stack
         addLeft(node.right)
+        /// return the value of node which was removed
         return node.val
     }
     
     func hasNext() -> Bool {
+        /// if stack is not empty than it has next
         return !stack.isEmpty
     }
 }
