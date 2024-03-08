97ddd366cdc88657764b4715d7b5d51fb306a8e9
Kishan Gupta <g.kis905@gmail.com>
2024-03-08 19:05:31 +0530

    Time: 159 ms (28.30%), Space: 17.4 MB (16.98%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.swift
+++ 0929-unique-email-addresses/0929-unique-email-addresses.swift
@@ -1,26 +1,14 @@
 class Solution {
     func numUniqueEmails(_ emails: [String]) -> Int {
         var set = Set<String>()
-
-        for item in emails {
-            var email: String = ""
-            var isPlus = false
-            for (index,char) in item.enumerated() {
-                if char == "@" {
-                    let domaim = item.suffix(item.count-index)
-                    email += String(domaim)
-                    break
-                } else if char == "." {
-                    continue
-                } else if isPlus || char == "+" {
-                    isPlus = true
-                    continue
-                }
-               email += String(char)
-            }
-            set.insert(email)
+        
+        for email in emails {
+            let split = email.components(separatedBy: "@")
+            let localName = split[0].components(separatedBy: "+").first!
+            let filtername = localName.components(separatedBy: ".").joined()
+            let newEmail = filtername+"@"+split.last!
+            set.insert(newEmail)
         }
-
         return set.count
     }
 }
