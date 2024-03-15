d169ac9bdccb69f1454c4d4e50bc32abc26ea0a8
Kishan Gupta <g.kis905@gmail.com>
2024-03-15 09:06:07 +0530

    Time: 101 ms (42.33%), Space: 23.7 MB (6.39%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.swift
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.swift
@@ -1,29 +1,20 @@
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var prefix:[Int] = nums
-        var postfix: [Int] = nums
-        var output: [Int] = []
-        let count = nums.count
+        var prefixProdut = 1
+        var suffixProduct = 1
+        var result: [Int] = []
 
-        for i in 0..<count {
-            if i != 0 {
-                prefix[i] = prefix[i]*prefix[i-1]
-            }
-
-            if count-i-1 != count-1 {
-                postfix[count-i-1] = postfix[count-i-1] * postfix[count-i]
-            }
+        for num in nums {
+            result.append(prefixProdut)
+            prefixProdut *= num
         }
 
-        for i in 0..<count {
-            if i == 0 {
-                output.append(postfix[i+1])
-            } else if i == count-1 {
-                output.append(prefix[i-1])
-            } else {
-                output.append(prefix[i-1]*postfix[i+1])
-            }
+        for i in (0..<nums.count).reversed() {
+            result[i] *= suffixProduct
+            suffixProduct *= nums[i]
+
         }
-        return output
+
+        return result
     }
 }
