0c9b79388284f99ecd99c8f051cff6d564dbd470
Kishan Gupta <g.kis905@gmail.com>
2024-03-26 22:21:18 +0530

    Time: 5 ms (46.40%), Space: 16.1 MB (60.01%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        var stack: [Character] = []
+
+        for char in s {
+            if char == "(" || char == "{" || char == "[" {
+                stack.append(char)
+            } else if char == ")" && stack.last == "(" {
+                stack.removeLast()
+            } else if char == "}" && stack.last == "{" {
+                stack.removeLast()
+            } else if char == "]" && stack.last == "[" {
+                stack.removeLast()
+            } else {
+                return false
+            }
+        }
+
+        return stack.isEmpty
+    }
+}
