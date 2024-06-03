e5f111070463cef3ed79572bf2b26e046a8d576a
Kishan Gupta <g.kis905@gmail.com>
2024-06-03 21:19:25 +0530

    Time: 25 ms (88.36%), Space: 16 MB (29.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.swift
+++ 0042-trapping-rain-water/0042-trapping-rain-water.swift
@@ -1,22 +1,23 @@
 class Solution {
     func trap(_ height: [Int]) -> Int {
+        var water = 0
         var i = 0
         var j = height.count-1
         var maxLeft = 0
         var maxRight = 0
-        var water = 0
 
         while i<j {
-            if height[i] <= height[j] {
-                maxLeft = max(maxLeft, height[i])
-                water += maxLeft-height[i]
-                i+=1
+            if height[i] < height[j] {
+                maxLeft = max(height[i], maxLeft)
+                water += height[i]-maxLeft
+                i += 1
             } else {
                 maxRight = max(maxRight, height[j])
-                water += maxRight-height[j]
-                j-=1
+                water += height[j]-maxRight
+                j -= 1
             }
         }
-        return water
+
+        return -water
     }
 }
