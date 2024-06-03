a24dc442f55c195bb8015dc021f4784128035207
Kishan Gupta <g.kis905@gmail.com>
2024-06-03 21:13:05 +0530

    Time: 208 ms (95.6%), Space: 19.3 MB (84.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.swift
+++ 0011-container-with-most-water/0011-container-with-most-water.swift
@@ -1,21 +1,22 @@
-class Solution {
+final class Solution {
     func maxArea(_ height: [Int]) -> Int {
         var i = 0
         var j = height.count-1
         var maxi = 0
 
-        while(i<j) {
-            let mini = min(height[i], height[j])
-            let capacity = (j-i)*mini
-            maxi = max(maxi, capacity)
-            
+        while i<j {
             if height[i] < height[j] {
+                let water = (j-i)*height[i]
+                maxi = max(water, maxi)
+                
                 i+=1
             } else {
+                let water = (j-i)*height[j]
+                maxi = max(water, maxi)
                 j-=1
             }
         }
-
+        
         return maxi
     }
 }
