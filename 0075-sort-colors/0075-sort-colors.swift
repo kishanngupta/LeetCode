a14c4ca259ae211327c3a6d313041b210ada10c2
Kishan Gupta <g.kis905@gmail.com>
2024-06-12 07:43:24 +0530

    Time: 5 ms (53.61%), Space: 15.8 MB (76.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.swift
+++ 0075-sort-colors/0075-sort-colors.swift
@@ -1,28 +1,20 @@
 class Solution {
     func sortColors(_ nums: inout [Int]) {
-        var low = 0
-        var medium = 0
-        var high = nums.count-1
+        var i = 0
+        var j = 0
+        var k = nums.count-1
 
-        while(medium <= high) {
-            if nums[medium] == 0 {
-                swap(low, medium)
-                medium += 1
-                low += 1
-            } else if nums[medium] == 1 {
-                medium += 1
+        while j<=k{
+            if nums[j] == 0 {
+                nums.swapAt(i, j)
+                i+=1
+                j+=1
+            } else if nums[j] == 1 {
+                j+=1
             } else {
-                swap(medium, high)
-                high -= 1
-            }
-        }
-
-        func swap(_ a: Int,_ b: Int) {
-            let temp = nums[a]
-            nums[a] = nums[b]
-            nums[b] = temp
+                nums.swapAt(j,k)
+                k-=1
+            }   
         }
     }
-
-    
 }
