402268103c59155d9008893f14a5a73d74fd62b9
Kishan Gupta <g.kis905@gmail.com>
2024-06-17 09:22:40 +0530

    Time: 2 ms (75%), Space: 15.3 MB (33.33%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.swift
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.swift
@@ -1,10 +1,16 @@
 class Solution {
     func judgeSquareSum(_ c: Int) -> Bool {
-        if c < 0 { return false }
-        if c == 0 || c == 1 || c == 2 { return true }
-        for i in 0...Int(sqrt(Double(c))) {
-            let val = sqrt(Double(c - i * i))
-            if val == Double(Int(val)) { return true }
+        var l = 0, r = Int(sqrt(Double(c)))
+
+        while l <= r {
+           var total = l * l + r * r 
+           if total > c {
+                r -= 1
+           } else if total < c {
+                l += 1
+           } else {
+            return true
+           }
         }
         return false
     }
