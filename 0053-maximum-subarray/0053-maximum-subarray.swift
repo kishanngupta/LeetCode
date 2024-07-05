dd883c8522267b59fccdf70fea5b676597d797ce
Kishan Gupta <g.kis905@gmail.com>
2024-07-05 17:45:00 +0530

    Time: 299 ms (31.36%), Space: 19.8 MB (57.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.swift
+++ 0053-maximum-subarray/0053-maximum-subarray.swift
@@ -2,15 +2,22 @@ class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
         var sum = 0
         var maxi = Int.min
+        var negative: Int? = nil
 
         for num in nums {
             sum += num
-            maxi = max(sum, maxi)
-
+            
             if sum < 0 {
                 sum = 0
+                if maxi == Int.min {
+                    negative = max(negative ?? Int.min, num)
+                }
+            } else {
+                negative = nil
+                maxi = max(maxi, sum)
             }
+            
         }
-        return maxi 
+        return negative != nil ? negative! : maxi
     }
 }
