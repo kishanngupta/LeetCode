2e5f7fb823cbb795cf6ed3e5cd540c454e59ac42
Kishan Gupta <g.kis905@gmail.com>
2024-07-05 19:26:27 +0530

    Time: 9 ms (6.82%), Space: 15.9 MB (66.19%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.swift
+++ 0048-rotate-image/0048-rotate-image.swift
@@ -1,13 +1,19 @@
 class Solution {
     func rotate(_ matrix: inout [[Int]]) {
 
-        for i in 0..<matrix.count {
-            for j in i+1..<matrix.count {
+        for i in matrix.indices {
+            for j in matrix[0].indices {
+                if j <= i {
                     let temp = matrix[i][j]
                     matrix[i][j] = matrix[j][i]
                     matrix[j][i] = temp
+                }
             }
+        }
+
+        for i in matrix.indices {
             matrix[i].reverse()
         }
+        
     }
 }
