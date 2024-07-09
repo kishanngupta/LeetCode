f7d8b36ee8235e3159ceb5d4d7ac1740c9efc4fd
Kishan Gupta <g.kis905@gmail.com>
2024-07-09 13:57:57 +0530

    Time: 22 ms (86.67%), Space: 17.3 MB (26.67%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.swift
+++ 0229-majority-element-ii/0229-majority-element-ii.swift
@@ -1,43 +1,44 @@
 class Solution {
     func majorityElement(_ nums: [Int]) -> [Int] {
-        var count1 = 1
-        var count2 = 0
-        var element1: Int? = nums.first!
+        var element1: Int? = nil
         var element2: Int? = nil
-        var result: [Int] = []
-        
-        for item in nums.dropFirst() {
-            if count1 == 0 && item != element2 {
-                element1 = item
+        var count1 = 0
+        var count2 = 0
+
+        for num in nums {
+            if count1 == 0 && element2 != num {
+                element1 = num
+                count1 = 1
+            } else if count2 == 0 && element1 != num {
+                element2 = num
+                count2 = 1
+            } else if element1 == num {
                 count1 += 1
-            } else if count2 == 0 && item != element1 {
-                element2 = item
-                count2 += 1
-            } else if item == element1 {
-                count1 += 1
-            } else if item == element2 {
+            } else if element2 == num {
                 count2 += 1
             } else {
                 count1 -= 1
                 count2 -= 1
             }
         }
-        
+
         count1 = 0
         count2 = 0
-        
-        for item in nums {
-            if item == element1 {
+
+        for num in nums {
+            if element1 == num {
                 count1 += 1
-            } else if item == element2 {
+            } else if element2 == num {
                 count2 += 1
             }
         }
-        
-        if (count1 > nums.count/3) {
+
+        print(count1, count2, element1, element2)
+
+        var result: [Int] = []
+        if count1 > nums.count/3 {
             result.append(element1!)
         }
-        
         if count2 > nums.count/3 {
             result.append(element2!)
         }
