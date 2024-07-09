e799175d80f15e133776ac4d58c4757a8540be22
Kishan Gupta <g.kis905@gmail.com>
2024-07-09 13:19:27 +0530

    Time: 446 ms (100%), Space: 25.1 MB (50%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func averageWaitingTime(_ customers: [[Int]]) -> Double {
+        var totalWait = customers[0][1]
+        var nextAvailableTime = customers[0][0] + customers[0][1]
+
+        for i in 1..<customers.count {
+            let customer = customers[i]
+
+            if nextAvailableTime <= customer[0] {
+                nextAvailableTime = customer[0] + customer[1]
+            } else {
+                nextAvailableTime += customer[1]
+                totalWait += (nextAvailableTime - (customer[0] + customer[1]))
+            }
+
+            totalWait += customer[1]
+
+        }
+
+        print(totalWait)
+
+        return Double(totalWait)/Double(customers.count)
+    }
+}
