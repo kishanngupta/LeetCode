1b0c2d0f2c41376a39faccb99d5014e2368889d6
Kishan Gupta <g.kis905@gmail.com>
2024-07-10 14:44:25 +0530

    Time: 22 ms (73.02%), Space: 16.7 MB (22.22%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.swift
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.swift
@@ -2,19 +2,20 @@ class Solution {
     func characterReplacement(_ s: String, _ k: Int) -> Int {
         var s = Array(s)
         var left = 0
-        var maxFreq = 0
+        var kk = 0
         var count = 0
+        var maxFreq = 0
         var hash: [Character: Int] = [:]
 
-        for right in 0..<s.count {
-            hash[s[right], default: 0] += 1
-            maxFreq = max(maxFreq, hash[s[right]]!)
+        for right in s.indices {
+            let char = s[right]
+            hash[char, default: 0] += 1
+            maxFreq = max(maxFreq, hash[char]!)
 
-            while right-left+1 > maxFreq + k {
+            while right-left+1 > maxFreq+k {
                 hash[s[left]]! -= 1
                 left += 1
             }
-
             count = max(count, right-left+1)
         }
 
