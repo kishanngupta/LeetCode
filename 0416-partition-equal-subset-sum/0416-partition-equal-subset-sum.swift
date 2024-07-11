7bc0b1cd9ca942f5c271a883b7592ef1c1270b9c
Kishan Gupta <g.kis905@gmail.com>
2024-07-11 12:46:45 +0530

    Time: 345 ms (11.23%), Space: 17.3 MB (18.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.swift
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.swift
@@ -1,17 +1,26 @@
 class Solution {
     func canPartition(_ nums: [Int]) -> Bool {
-        let sum = nums.reduce(0, +)
-        if sum % 2 != 0 {
-            return false
-        }
-        let target = sum / 2
-        var dp = Array(repeating: false, count: target + 1)
-        dp[0] = true
-        for num in nums {
-            for i in stride(from: target, through: num, by: -1) {
-                dp[i] = dp[i] || dp[i - num]
+        var sum = nums.reduce(0,+)
+        if sum%2 == 1 { return false }
+        sum = sum/2
+        var dp: [[Bool?]] = Array(repeating: Array(repeating: nil, count: sum+1), count: nums.count)
+
+        func partition(_ i: Int,_ sum: Int) -> Bool {
+            if i == nums.count-1 {
+                return nums[i] == sum
+            } else if sum < 0 {
+                return false
+            } else if dp[i][sum] != nil {
+                return dp[i][sum]!
             }
+
+            let notPick = partition(i+1, sum)
+            let pick = partition(i+1, sum-nums[i])
+
+            dp[i][sum] = pick || notPick
+            return dp[i][sum]!
         }
-        return dp[target]
+
+        return partition(0,sum)        
     }
 }
